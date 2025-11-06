b021b677e6b92f893db06fc2ff486bb60f2bc061
Rob Lee <robl@rjlee.net>
2025-11-06 00:49:35 +0000

    fix: Standardised jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,26 @@
-/** @type {import('jest').Config} */
-module.exports = {
+const collectCoverage = process.env.JEST_COVERAGE === 'true';
+
+const config = {
   testEnvironment: 'node',
   roots: ['<rootDir>/src', '<rootDir>/tests'],
-  collectCoverage: true,
+  testMatch: ['**/tests/**/*.test.js'],
+  setupFiles: ['<rootDir>/jest.setup.js'],
+  passWithNoTests: true,
+  verbose: process.env.JEST_VERBOSE === 'true',
+  collectCoverage,
   coverageDirectory: 'coverage',
-  coverageThreshold: {
+  testPathIgnorePatterns: ['/node_modules/'],
+};
+
+if (collectCoverage) {
+  config.coverageThreshold = {
     global: {
       branches: 50,
       functions: 70,
       lines: 80,
       statements: 80,
     },
-  },
-  testMatch: ['**/tests/**/*.test.js'],
-};
+  };
+}
+
+module.exports = config;
